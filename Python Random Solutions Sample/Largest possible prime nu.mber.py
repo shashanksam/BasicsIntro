41cfd0b271894e770f46058d09357b7a885b9412
ShashankSamS <35063506+shashanksam@users.noreply.github.com>
2024-01-04 17:16:14 +0530

    Create Largest possible prime nu.mber.py
    Largest possible prime number incomplete

---
+++ Python Random Solutions Sample/Largest possible prime nu.mber.py
@@ -0,0 +1,49 @@
+IAP
+Level 1
+01
+Hours
+:
+29
+Minutes
+
+
+Question List
+Largest possible prime nu...
+0%
+Bouncy Number Series
+0%
+Abundant Number
+0%
+
+Problem Statement
+Given a positive integer N having D digits, write a program to print the largest possible prime number which can be formed by using the same set of digits as N. Print -1 if it is not possible to form a prime number.
+
+Input Format
+
+A single line containing the positive integer N. Read the input from standard input
+
+Constraints
+
+N >0
+Output Format
+
+Print the largest possible prime number. Print -1 if prime number cannot be formed. Print the output to standard output stream
+
+Sample Input	Sample Output	Explanation
+7259	9257	Out of all possible shuffled numbers, 9257 is the largest possible prime number.
+
+
+def make_number_prime(num):
+    # write your code here
+    pass
+​
+​
+​
+num = int(input())
+print(make_number_prime(num))
+​
+
+          
+
+
+
