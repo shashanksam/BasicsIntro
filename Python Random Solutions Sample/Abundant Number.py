8d6dbc719c3231e5ef284e76c0d84cde6c977356
ShashankSamS <35063506+shashanksam@users.noreply.github.com>
2024-01-04 17:17:23 +0530

    Create Abundant Number.py incomplete
    Abundant Number incomplete

---
+++ Python Random Solutions Sample/Abundant Number.py
@@ -0,0 +1,38 @@
+IAP
+Level 1
+01
+Hours
+:
+29
+Minutes
+
+
+Question List
+Largest possible prime nu...
+0%
+Bouncy Number Series
+0%
+Abundant Number
+0%
+
+Problem Statement
+Write a program to check if the number is an abundant number. A number is said to be abundant if the sum of proper divisors is greater than the number itself.
+
+Input and Output Format :
+
+Read the number from standard input and print your output to the standard output.
+
+The input contains an integer. Output True if it is an abundant number and False if it is not.
+
+Sample Input	Sample Output	Explanation
+12	True	12 is an abundant number
+121	False	121 is not an abundant number
+
+number = input()
+print(is_abundant(int(number)))
+​
+
+          
+
+
+
