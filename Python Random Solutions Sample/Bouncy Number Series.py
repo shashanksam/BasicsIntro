ee5ff5b3805d6b47643aace2809ccd84fce24831
ShashankSamS <35063506+shashanksam@users.noreply.github.com>
2024-01-04 17:16:47 +0530

    Create Bouncy Number Series.py
    Bouncy Number Series incomplete

---
+++ Python Random Solutions Sample/Bouncy Number Series.py
@@ -0,0 +1,39 @@
+IAP
+Level 1
+01
+Hours
+:
+29
+Minutes
+
+
+Question List
+Largest possible prime nu...
+0%
+Bouncy Number Series
+0%
+Abundant Number
+0%
+
+Problem Statement
+Bouncy number is a positive integer which has its digits un-ordered. 14752 is a bouncy number where 95321 is not a bouncy number. 
+
+Given a positive number N, write a program to print the total number of bouncy numbers that exists from 0 to N (including N).
+
+Input and Output Format :
+
+Read the number from standard input stream and print the output to the standard output.
+
+Sample Input	Sample Output	Explanation
+110	9	Bouncy Numbers are 101,102,103…109
+121	11	Bouncy Numbers are 101,102,103…109 and 120,121
+
+
+N = int(input())
+print (count_bouncy_numbers(N))
+​
+
+          
+
+
+
